abeb0252ca30768ebb7e1713c598c932a3533aa9
Vijay-Anandharaman <anantharamanvijay@gmail.com>
2023-09-10 20:32:45 +0530

    ASCII Value Program

---
+++ Java Workspace/Basic Programs/ASCIIValue.java
@@ -0,0 +1,12 @@
+package in.workspace.main;
+
+public class ASCIIValue
+{
+	public static void main(String[] args)
+    {
+		for(int i = 0; i<=255; i++)
+		{
+			System.out.println("The ASCII value of " + (char)i + " = " + i);
+		}  
+	}
+}
