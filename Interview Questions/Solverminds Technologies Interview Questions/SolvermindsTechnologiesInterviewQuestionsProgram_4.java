74dacdb45cf681f3b4e6653e10e048c1032310eb
Vijay-Anandharaman <anantharamanvijay@gmail.com>
2023-09-29 12:47:05 +0530

    Solverminds Technologies Interview Questions Programs

---
+++ Interview Questions/Solverminds Technologies Interview Questions/SolvermindsTechnologiesInterviewQuestionsProgram_4.java
@@ -0,0 +1,13 @@
+package in.workspace.main;
+
+public class SolvermindsTechnologiesInterviewQuestionsProgram_4
+{	
+	public static void main(String[] args)
+    {	
+		int[] iok = {22, 23, 100, 50};
+		
+		for(int i : iok)
+		
+		System.out.println(i);
+	}
+}
