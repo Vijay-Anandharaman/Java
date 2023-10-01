2e808c148713270844f623fd080233d8077951bd
Vijay-Anandharaman <anantharamanvijay@gmail.com>
2023-10-01 11:56:15 +0530

    Solverminds Technologies Interview Questions Programs

---
+++ Interview Questions/Solverminds Technologies Interview Questions/SolvermindsTechnologiesInterviewQuestionsProgram_6.java
@@ -0,0 +1,13 @@
+package in.workspace.main;
+
+public class SolvermindsTechnologiesInterviewQuestionsProgram_6
+{
+    public static void main(String[] args)
+    {		
+		short x = 5;
+		
+		x = x * 10;
+		
+		System.out.println(x);
+	}
+}
