9eeac5b05a5950d3af9343be69a19b47bb106890
Vijay-Anandharaman <anantharamanvijay@gmail.com>
2023-10-13 18:46:28 +0530

    Alphabetical Q Pattern Program

---
+++ Java Workspace/Pattern Programs/Alphabetical A-Z Pattern Programs/Alphabetical_Q_PatternProgram.java
@@ -0,0 +1,37 @@
+package in.workspace.main;
+
+import java.util.Scanner;
+
+public class Alphabetical_Q_PatternProgram 
+{
+	public static void main(String[] args)
+    {		
+		Scanner pattern = new Scanner(System.in);
+
+		System.out.print("Enter the number : ");
+
+		int n = pattern.nextInt();
+
+		System.out.println();
+
+		if(n%2==0)
+		{
+			n=n+1;
+		}
+		for(int i=0;i<n;i++)
+		{
+			for(int j=0;j<n;j++)
+			{
+				if((j>n/4&&i==n/4&&j<(3*n)/4) || (i>n/4&&j==n/4&&i<(3*n)/4) || (j>n/4&&i==(3*n)/4&&j<(3*n)/4) || (i>n/4&&j==(3*n)/4&&i<(3*n)/4) || (j>(n-1)/2&&i==j&&j<n-1))
+				{
+					System.out.print("*");
+				}
+				else
+				{
+					System.out.print(" ");
+				}								
+		    }
+			System.out.println("");			
+		}
+	}
+}
