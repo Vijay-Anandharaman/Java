b81dd0e87bee5ff955d8c04c1089e3c0f07031b2
Vijay-Anandharaman <anantharamanvijay@gmail.com>
2023-10-22 14:31:54 +0530

    Alphabetical Z Pattern Program

---
+++ Java Workspace/Pattern Programs/Alphabetical A-Z Pattern Programs/Alphabetical_Z_PatternProgram.java
@@ -0,0 +1,37 @@
+package in.workspace.main;
+
+import java.util.Scanner;
+
+public class Alphabetical_Z_PatternProgram
+{
+	public static void main(String[] args)
+    {		
+		Scanner pattern = new Scanner(System.in);
+
+		System.out.print("Enter the number : ");
+
+		int n = pattern.nextInt();
+
+		System.out.println();
+
+		if(n%2==0)
+		{
+			n=n+1;
+		}
+		for(int i=0;i<n;i++)
+		{
+			for(int j=0;j<n;j++)
+			{
+				if(i==0 || i+j==n-1 || i==n-1)
+				{
+					System.out.print("*");
+				}
+				else
+				{
+					System.out.print(" ");
+				}								
+		    }
+			System.out.println("");			
+		}
+	}
+}
