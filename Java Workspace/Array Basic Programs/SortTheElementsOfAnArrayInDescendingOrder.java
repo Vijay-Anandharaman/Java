40090b6fab96e6e086cd1762a7c9b7b117970589
Vijay-Anandharaman <anantharamanvijay@gmail.com>
2023-11-09 09:54:35 +0530

    Sort The Elements Of An Array In Descending Order Program

---
+++ Java Workspace/Array Basic Programs/SortTheElementsOfAnArrayInDescendingOrder.java
@@ -0,0 +1,40 @@
+package in.workspace.main;
+
+public class SortTheElementsOfAnArrayInDescendingOrder
+{
+	public static void main(String[] args)
+    {		
+		int numbers[] = {2,5,1,3,4};
+		
+		int temporaryValues = 0;
+		
+		System.out.print("Elements in given array : ");
+		
+		for(int i=0;i<numbers.length;i++)
+		{
+			System.out.print(numbers[i] + " ");
+		}
+		
+		for(int i=0;i<numbers.length;i++)
+		{
+			for(int j=i+1;j<numbers.length;j++)
+			{
+				if(numbers[i]<numbers[j])
+				{
+					temporaryValues = numbers[i];
+					numbers[i] = numbers[j];
+					numbers[j] = temporaryValues;
+				}
+			}
+		}
+		
+		System.out.println();
+		
+		System.out.print("Elements of an array in descending order : ");
+		
+		for(int i=0;i<numbers.length;i++)
+		{
+			System.out.print(numbers[i] + " ");
+		}
+	}
+}
