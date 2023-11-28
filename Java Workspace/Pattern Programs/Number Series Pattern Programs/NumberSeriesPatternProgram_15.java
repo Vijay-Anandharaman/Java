975b8efecd22eb8eb31f5df53764b4388c819bae
Vijay-Anandharaman <anantharamanvijay@gmail.com>
2023-11-28 13:18:22 +0530

    Number Series Pattern Program-15

---
+++ Java Workspace/Pattern Programs/Number Series Pattern Programs/NumberSeriesPatternProgram_15.java
@@ -0,0 +1,16 @@
+package in.workspace.main;
+
+public class NumberSeriesPatternProgram_15
+{
+    public static void main(String args[])
+    {
+        for(int i=9;i>=1;i--)
+        {
+            for(int j=1;j<=i;j++)
+            {
+                System.out.print(j + " ");
+            }
+            System.out.println();
+        }
+    }
+}
