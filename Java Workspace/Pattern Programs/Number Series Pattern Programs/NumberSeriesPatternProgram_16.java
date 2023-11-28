f2211603634534778710dac1739ccfd5361ffd54
Vijay-Anandharaman <anantharamanvijay@gmail.com>
2023-11-28 13:25:17 +0530

    Number Series Pattern Program-16

---
+++ Java Workspace/Pattern Programs/Number Series Pattern Programs/NumberSeriesPatternProgram_16.java
@@ -0,0 +1,16 @@
+package in.workspace.main;
+
+public class NumberSeriesPatternProgram_16
+{
+    public static void main(String args[])
+    {
+        for(int i=7;i>=1;i--)
+        {
+            for(int j=i;j>=1;j--)
+            {
+                System.out.print(j + " ");
+            }
+            System.out.println();
+        }
+    }
+}
